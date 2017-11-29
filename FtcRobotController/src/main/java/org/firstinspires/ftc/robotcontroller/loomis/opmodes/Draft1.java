1d4d812d3739f31b11a762dcfe8bd392b6d22f4c
Peyton <peyton.sandridge@gmail.com>
2017-11-28 16:51:33 -0800

    adding lift and range sensor

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/loomis/opmodes/Draft1.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/loomis/opmodes/Draft1.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.robotcontroller.loomis.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
@@ -8,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
  * Created by peyto on 10/25/2017.
  */
 
-public class Draft1 extends OpMode{
+public class Draft1 extends OpMode {
 
     DcMotor rotatingIntake;
     DcMotor stationaryIntake;
@@ -17,8 +18,10 @@ public class Draft1 extends OpMode{
     DcMotor frontRightDrive;
     DcMotor backLeftDrive;
     DcMotor backRightDrive;
+    CRServo leftLift;
+    CRServo rightLift;
 
-    double fast_speed = 1. / 2;
+    double fast_speed = 4. / 10;
     double slow_speed = 1. / 10;
 
     @Override
@@ -26,16 +29,17 @@ public class Draft1 extends OpMode{
         rotatingIntake = hardwareMap.get(DcMotor.class, "rotatingIntake");
         stationaryIntake = hardwareMap.get(DcMotor.class, "stationaryIntake");
         conveyor = hardwareMap.get(DcMotor.class, "conveyor");
-        frontLeftDrive  = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRightDrive = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeftDrive  = hardwareMap.get(DcMotor.class, "backLeft");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeft");
         backRightDrive = hardwareMap.get(DcMotor.class, "backRight");
-        //backLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftLift = hardwareMap.get(CRServo.class, "leftLift");
+        rightLift = hardwareMap.get(CRServo.class, "rightLift");
         frontLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
-
     }
+
 
     @Override
     public void loop() {
@@ -48,22 +52,44 @@ public class Draft1 extends OpMode{
 
 
         //main drive (Strafe) (Right Joystick)
-        double w = -gamepad1.right_stick_x*fast_speed;
-        double y = gamepad1.left_stick_y*fast_speed;
-        double x = gamepad1.left_stick_x*fast_speed;
-        double dy = (gamepad1.dpad_down ? -slow_speed : 0 ) + (gamepad1.dpad_up ? slow_speed : 0);
+        double w = -gamepad1.right_stick_x * fast_speed;
+        double y = gamepad1.left_stick_y * fast_speed;
+        double x = gamepad1.left_stick_x * fast_speed;
+        double dy = (gamepad1.dpad_down ? -slow_speed : 0) + (gamepad1.dpad_up ? slow_speed : 0);
         double dx = (gamepad1.dpad_left ? -slow_speed : 0) + (gamepad1.dpad_right ? slow_speed : 0);
 
 
-        backLeftDrive.setPower(y-x-w-dx+dy);
-        frontLeftDrive.setPower(x+y-w+dx+dy);
-        backRightDrive.setPower(x+y+w+dx+dy);
-        frontRightDrive.setPower(y-x+w-dx+dy);
+        backLeftDrive.setPower(y - x - w - dx + dy);
+        frontLeftDrive.setPower(x + y - w + dx + dy);
+        backRightDrive.setPower(x + y + w + dx + dy);
+        frontRightDrive.setPower(y - x + w - dx + dy);
 
-//        backLeftDrive.setPower(gamepad1.left_stick_y-gamepad1.left_stick_x-w);
-//        frontLeftDrive.setPower(gamepad1.left_stick_x+gamepad1.left_stick_y-w);
-//        backRightDrive.setPower(gamepad1.left_stick_x+gamepad1.left_stick_y+w);
-//        frontRightDrive.setPower(gamepad1.left_stick_y-gamepad1.left_stick_x+w);
+        if (gamepad1.a) expanding();
+        if (gamepad1.b) shrinking();
 
+
+    }
+
+    public void expanding() {
+        leftLift.setPower(1);
+        rightLift.setPower(-1);
+        sleep(250);
+        leftLift.setPower(0);
+        rightLift.setPower(0);
+    }
+    public void shrinking(){
+        leftLift.setPower(-1);
+        rightLift.setPower(1);
+        sleep(250);
+        leftLift.setPower(0);
+        rightLift.setPower(0);
+    }
+
+    public void sleep(int sleeptime) {
+        try {
+            Thread.sleep(sleeptime, 0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
